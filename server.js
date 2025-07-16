a680c439122df17ea14d1d3d50c87223d465c503
Madeline Williams <73550645+maddiethegm@users.noreply.github.com>
2025-07-16 01:57:33 -0400

    Wip (#4)
    * Added username only logging for logins and adjusted transaction logging param to accommodate. Added automatic creation of .env files if they aren't present when the main server.js starts.
    
    ---------
    
    Co-authored-by: Maddie <maddiethegm97@gmail.com>

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // server.js
-require('dotenv').config();
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const sql = require('mssql');
+require('./setEnv');
 /**
  * Main entry point for the application.
  */
-
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 const cors = require('cors');
@@ -27,11 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
 /**
  * CORS middleware configuration.
  */
-app.use(cors({
-  origin: 'http://localhost:3000', // Allow requests from this origin
-  methods: 'GET,POST,PUT,PATCH,DELETE',
-  allowedHeaders: 'Content-Type,Authorization'
-}));
+app.use(cors());
 
 // SQL Server Configuration
 const config = {
---
+++ setEnv.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const defaultEnvContent = `
+### API Server Configuration
+#### Save these variables in /.env
+The following environment variables are used to configure the application:
+PORT=
+
+#    API server listening port.
+#    Default: 3001
+### DB Config
+DB_USER=homeinvuser
+#
+#    DB username for read/write access
+DB_PASSWORD=homeinvpassword
+
+#    Password to login as DB_USER
+DB_SERVER=localhost
+#
+#    DB server URL
+#    Example: sql.example.tld
+#    Example: localhost
+DB_DATABASE=homeinv
+#
+#    Your database name
+DB_TYPE=MSSQL
+#
+#    Options: mssql, oracle, mariadb, postgres
+### JWT Configuration
+JWT_SECRET=YOURSECRETKEY
+#
+#    Example: your_secret_key
+TOKEN_EXPIRY='1h'
+#
+#    Example: '8h'
+#    Default: '1h'
+### LDAP Config
+LDAP_URL=ldap://localhost
+#
+#    Example: ldap://example.tld
+LDAP_DOMAIN_COMPONENTS=
+#
+#    Example: 'cn=users,dc=example,dc=tld'
+LDAP_USER_ATTRIBUTE='cn=users,dc=example,dc=tld'
+#
+#    Example: 'cn'
+#`;
+
+const defaultClientEnvContent = `
+### React App Config
+#Save these variables client folder - default /client/.env
+### API Config
+REACT_APP_API_URL=http://localhost:3001
+#
+#    URL to reach your API backend. This is relative to your client, not the frontend server.
+#    Example: http://homeinv.example.tld/api:3001
+### Footer details
+REACT_APP_SUPPORT_CONTACT=admin@example.mail
+#    
+#    Example: "Me"
+#    Example: "admin@example.tld"
+#REACT_APP_FOOTER_IMG_URL=http://example.tld/footer.png
+#
+#    Example: http://example.tld/footer.png
+#`;
+const envPath = process.env.ENV_PATH || './.env';
+const clientEnvPath = process.env.CLIENT_ENV_PATH || './client/.env'
+if (!fs.existsSync(envPath)) {
+    console.log('.env file not found, creating one with default values...');
+    fs.writeFileSync(envPath, defaultEnvContent);
+} else {
+    console.log('.env file already exists.');
+}
+if (!fs.existsSync(clientEnvPath)) {
+    console.log('.env file not found, creating one with default values...');
+    fs.writeFileSync(clientEnvPath, defaultClientEnvContent);
+} else {
+    console.log('./client/.env file already exists.');
+}
